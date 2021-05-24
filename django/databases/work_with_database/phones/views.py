ab5d395731425d357d93e9936f767af95b48ea5b
pochtalexa <pochtalexa@yandex.ru>
2021-05-24 17:46:22 +0300

    work_with_database corr

--- django/databases/work_with_database/phones/views.py
+++ django/databases/work_with_database/phones/views.py
@@ -9,21 +9,20 @@ from urllib.parse import urlencode
 def show_catalog(request):
     template = 'catalog.html'
     order_by = request.GET.get('order_by', None)
-    show_catalog.counter += 1
 
     if order_by in ['name', 'release_date', 'lte_exists', 'slug']:
         phones = Phone.objects.all().order_by(order_by)
-    elif order_by == 'price':
-        if show_catalog.counter % 2:
-            phones = Phone.objects.all().order_by(order_by)
-        else:
-            phones = Phone.objects.all().order_by('-' + order_by)
+    elif order_by == 'price_up':
+        phones = Phone.objects.all().order_by('price')
+    elif order_by == 'price_down':
+        phones = Phone.objects.all().order_by('-' + 'price')
     else:
         phones = Phone.objects.all()
 
     sort_links = {
         'name': f"{reverse('phone_catalog')}?{urlencode({'order_by': 'name'})}",
-        'price': f"{reverse('phone_catalog')}?{urlencode({'order_by': 'price'})}",
+        'price_up': f"{reverse('phone_catalog')}?{urlencode({'order_by': 'price_up'})}",
+        'price_down': f"{reverse('phone_catalog')}?{urlencode({'order_by': 'price_down'})}",
         'release_date': f"{reverse('phone_catalog')}?{urlencode({'order_by': 'release_date'})}",
         'lte_exists': f"{reverse('phone_catalog')}?{urlencode({'order_by': 'lte_exists'})}",
         'slug': f"{reverse('phone_catalog')}?{urlencode({'order_by': 'slug'})}"
@@ -39,7 +38,13 @@ def show_catalog(request):
 
 def show_product(request, slug):
     template = 'product.html'
-    phone = Phone.objects.filter(slug=slug)[0]
+
+    slugs = Phone.objects.values_list('slug', flat=True)
+
+    if slug in slugs:
+        phone = Phone.objects.filter(slug=slug)[0]
+    else:
+        return show_catalog(request)
     context = {'data': phone}
     return render(request, template, context)
 
@@ -47,6 +52,3 @@ def show_product(request, slug):
 def import_phones_view(request):
     phones = import_phones.Command().handle()
     return HttpResponse('Импорт завершен')
-
-
-show_catalog.counter = 0
